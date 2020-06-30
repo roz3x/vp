4b3fb5d9586154f3e557c98988d1ce3990cfcf70
roz3x <oroz3x@gmail.com>
2020-06-30 11:31:10 +0000

    files

---
+++ s.asm
@@ -0,0 +1,24 @@
+SET 0 1
+SET 1 2
+SET 2 3
+SET 3 4
+SET 4 5
+SET 5 6
+SET 6 7
+SET 7 80
+
+
+# upper bound
+MOV 1 8
+MOV 5 8
+
+#lower bound 
+MOV 2 0
+
+#search item
+MOV 3 4
+
+XCMP
+JMP
+PRINT
+EXIT
